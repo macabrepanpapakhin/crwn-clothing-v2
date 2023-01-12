6ca6d7c56d8814402aa383f046ed54ba6bae483f
Sai La Min Oak <sailaminoak@edu.mm>
2023-01-12 12:15:45 +0630

    tt

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,26 +1,13 @@
-import { useContext } from "react";
-import { Fragment } from "react/cjs/react.production.min";
-import ProductCard from "../../components/product-card/product-card.component";
-import { CategoriesContext } from "../../contexts/categories.context";
+import { Route, Routes } from "react-router-dom";
+import CategoriesPreview from "../category-preview/category-preview.component";
+import Category from "../category/category.comoponent";
 import "./shop.styles.scss";
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
-
   return (
-    <Fragment>
-      {Object.keys(categoriesMap).map((title) => {
-        return (
-          <Fragment key={title}>
-            <h2>{title}</h2>
-            <div className="products-container">
-              {categoriesMap[title].map((product) => (
-                <ProductCard product={product} key={product.id} />
-              ))}
-            </div>
-          </Fragment>
-        );
-      })}
-    </Fragment>
+    <Routes>
+      <Route index element={<CategoriesPreview />}></Route>
+      <Route path=":category" element={<Category />}></Route>
+    </Routes>
   );
 };
 
