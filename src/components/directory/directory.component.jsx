db7b8045ea7b547dcc45f11fe3cf865dd9ef537e
Sai La Min Oak <sailaminoak@edu.mm>
2023-01-12 12:35:17 +0630

    tt

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,10 +1,10 @@
-import CategoryItem from "../category-item/category-item.component";
+import DirectoryItem from "../directory-item/directory-item.component";
 
 const Directory = ({ categories }) => {
   return (
     <div className="categories-container">
       {categories.map(({ title, imageUrl, id }) => (
-        <CategoryItem title={title} imageUrl={imageUrl} key={id} />
+        <DirectoryItem title={title} imageUrl={imageUrl} key={id} />
       ))}
     </div>
   );
--- src/routes/category/category.comoponent.jsx
+++ src/routes/category/category.comoponent.jsx
@@ -3,6 +3,7 @@ import "./category.styles.scss";
 import { useContext, useState, useEffect } from "react";
 import { CategoriesContext } from "../../contexts/categories.context";
 import ProductCard from "../../components/product-card/product-card.component";
+import { Fragment } from "react/cjs/react.production.min";
 const Category = () => {
   // useParams returns object
   const { category } = useParams();
@@ -14,12 +15,15 @@ const Category = () => {
   }, [category, categoriesMap]);
 
   return (
-    <div className="category-container">
-      {products &&
-        products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-    </div>
+    <Fragment>
+      <h2 className="title">{category.toUpperCase()}</h2>
+      <div className="category-container">
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </Fragment>
   );
 };
 
