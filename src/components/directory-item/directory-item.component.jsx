e3c11376a0e9b9300717f332ed1b540431de3afe
Sai La Min Oak <sailaminoak@edu.mm>
2023-01-16 21:34:27 +0630

    tt

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,15 +1,23 @@
+import { Link } from "react-router-dom";
 import "./directory-item.style.scss";
 
-const DirectoryItem = ({ title, imageUrl }) => {
+const DirectoryItem = ({ title, imageUrl, route }) => {
   return (
     <div className="directory-item-container">
-      <div
+      <Link
+        to={route}
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
-      />
+      >
+        {" "}
+        <div />
+      </Link>
+
       <div className="directory-item-body">
-        <h2>{title}</h2>
-        <p>Shop Now</p>
+        <Link to={route}>
+          <h2>{title}</h2>
+          <p>Shop Now</p>
+        </Link>
       </div>
     </div>
   );
