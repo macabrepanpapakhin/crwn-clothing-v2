ca93e265bea96aa2f66b62de12b03fe29c4f0548
Sai La Min Oak <sailaminoak@edu.mm>
2023-01-17 20:24:56 +0630

    tt

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,23 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./directory-item.style.scss";
 
 const DirectoryItem = ({ title, imageUrl, route }) => {
+  const navigate = useNavigate();
+  const navigationHandler = () => navigate(route);
   return (
-    <div className="directory-item-container">
-      <Link
-        to={route}
+    <div className="directory-item-container" onClick={navigationHandler}>
+      <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
-      >
-        {" "}
-        <div />
-      </Link>
+      ></div>
 
       <div className="directory-item-body">
-        <Link to={route}>
-          <h2>{title}</h2>
-          <p>Shop Now</p>
-        </Link>
+        <h2>{title}</h2>
+        <p>Shop Now</p>
       </div>
     </div>
   );
--- src/contexts/users.context.jsx
+++ src/contexts/users.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import {
   onAuthStateChangedListener,
   SignOutUser,
@@ -8,9 +8,32 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USERS_ACTIONS_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USERS_ACTIONS_TYPES.SET_CURRENT_USER:
+      return { ...state, currentUser: payload };
+    default:
+      throw new Error(`Unhandled type ${type} in user reducer.`);
+  }
+};
+
+const INITIAL_STATE = { currentUser: null };
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  //SignOutUser();
+  // const [currentUser, setCurrentUser] = useState(null);
+  // SignOutUser();
+  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  const { currentUser } = state;
+
+  const setCurrentUser = (user) => {
+    dispatch({ type: USERS_ACTIONS_TYPES.SET_CURRENT_USER, payload: user });
+  };
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
       //console.log("in the user context");
